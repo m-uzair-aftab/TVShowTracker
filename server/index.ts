51bc06380a904e0f745a3a95d0d746b3d2349a06
uzairafab <m.uzair.aftab@gmail.com>
2025-08-10 10:39:54 -0700

    Fix: change the host binding back to 0.0.0.0

--- server/index.ts
+++ server/index.ts
@@ -111,7 +111,7 @@ app.use((req, res, next) => {
   const port = Number(process.env.PORT) || 5000;
   server.listen({
     port,
-    host: "localhost",
+    host: "0.0.0.0",
     reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
