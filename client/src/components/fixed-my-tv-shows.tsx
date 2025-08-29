9aa89e7868a96d5e8f3e52611cfa5374f7f60695
uzairafab <m.uzair.aftab@gmail.com>
2025-08-28 17:57:10 -0700

    Adding filters for 2023 and 2022 in the year dropdown menu.

--- client/src/components/fixed-my-tv-shows.tsx
+++ client/src/components/fixed-my-tv-shows.tsx
@@ -375,6 +375,8 @@ export function MyTVShows({ onSwitchToSearch }: MyTVShowsProps) {
                 <SelectItem value="all">All Years</SelectItem>
                 <SelectItem value="2025">2025</SelectItem>
                 <SelectItem value="2024">2024</SelectItem>
+                <SelectItem value="2023">2023</SelectItem>
+                <SelectItem value="2022">2022</SelectItem>
               </SelectContent>
             </Select>
           </div>
